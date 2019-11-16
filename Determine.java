fc8eb685af7af699b0d5dbdc5b1a34e36051e5de
Amber Johnson <amber.johnson@student.manchester.ac.uk>
2019-11-16 16:29:53 +0000

    Implemented calculateMoodScore method

--- Determine.java
+++ Determine.java
@@ -10,25 +10,37 @@ public class Determine {
     info.put("Happiness", 0.2);
     info.put("Surprise", 0.5);
     info.put("Neutral", 0.6);
-    info.put("Digust", 0.73);
+    info.put("Disgust", 0.73);
     info.put("Sadness", 0.12);
     info.put("Anger", 0.32);
     info.put("Contempt", 0.43);
     info.put("Fear", 0.54);
 
-    calculateHappy(info);
-    calculateNeutral(info);
-    calulateSad(info);
+    int mood = calculateMoodScore(info);
+    System.out.println(mood);
   } // main
 
-  public static double calculateHappy(Dictionary<String, Double> info){
-    //TODO
+  public static int calculateMoodScore(Dictionary<String, Double> info){
+    int moodScore = 0;
+    double totalHappy = info.get("Happiness") + info.get("Surprise");
+    double totalSad = info.get("Disgust") + info.get("Sadness")+ info.get("Anger")
+                      + info.get("Contempt") + info.get("Fear");
+    double avgHappy = totalHappy / 2;
+    double avgNeutral = info.get("Neutral");
+    double avgSad = totalSad / 5;
+    System.out.println(avgHappy);
+    System.out.println(avgNeutral);
+    System.out.println(avgSad);
+
+    if(avgHappy > avgNeutral && avgHappy > avgSad)
+        {
+            moodScore++;
+        }
+        else if(avgNeutral < avgSad)
+        {
+            moodScore--;
+        }
+    return moodScore;
   } // calculateHappy
 
-  public static double calculateNeutral(Dictionary<String, Double> info){
-    //TODO
-  } // calculateHappy
-  public static double calculateSad(Dictionary<String, Double> info){
-    //TODO
-  } // calculateHappy
 } // Determine
