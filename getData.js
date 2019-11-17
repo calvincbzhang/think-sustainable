5e5f1808e8d60ef190380160a3024c5a90843252
mohamedben0203 <mohamedben0203@gmail.com>
2019-11-17 06:04:42 +0000

    cumalitive

---
+++ getData.js
@@ -0,0 +1,38 @@
+var totalQuantity = 0;
+var totalFootPrint = 0;
+var averageHealth = 0;
+
+function printMe(barcode) {
+    var params = {
+        // Request parameters
+        "gtin": barcode,
+    };
+
+    $.ajax({
+        url: "https://dev.tescolabs.com/product/?" + $.param(params),
+        beforeSend: function(xhrObj){
+            // Request headers
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","82a19d5b25574c0491ea06ca5d8eadb2");
+        },
+        type: "GET",
+        // Request body
+        data: "{body}",
+    })
+    .done(function(data) {
+        const readableData = data.products[0].productCharacteristics
+        var healthScore = readableData.healthScore;
+        var hazard = readableData.isHazardous;
+        var quantity = data.products[0].qtyContents.quantity;
+        var footprint = quantity * 2.71;
+        averageHealth = (healthScore + averageHealth * totalQuantity) / (totalQuantity + 1);
+        totalQuantity++;
+        totalFootPrint = totalFootPrint + footprint;
+        console.log("quantity: " + totalQuantity);
+        console.log("total Foot Print: " + totalFootPrint);
+        console.log("healthScore: " + averageHealth);
+        console.log("is it Hazardous: " + hazard);
+    })
+    .fail(function() {
+        alert("The barcode is invalid, please try again");
+    });
+};
